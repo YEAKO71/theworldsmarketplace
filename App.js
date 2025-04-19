473392a4e404e57ee4ae561bc0b01d53995cece1
YEAKO71 <pyeakehson01@gmail.com>
2025-04-18 20:13:54 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-
 import React from "react";
-import logo from "./logo_theworldmarketplace.png";
+import logo from "./logo.png";
 
 export default function App() {
   return (
     <div style={{ padding: "2rem", maxWidth: "600px", margin: "0 auto", textAlign: "center" }}>
-      <img src={logo} alt="TheWorldsMarketplace Logo" style={{ width: "100px", marginBottom: "20px" }} />
+      <img src={logo} alt="Logo" style={{ width: "100px", marginBottom: "20px" }} />
       <h1>TheWorldsMarketplace</h1>
-      <p>Welcome to your new global marketplace!</p>
+      <p>Welcome to your global marketplace!</p>
     </div>
   );
 }
